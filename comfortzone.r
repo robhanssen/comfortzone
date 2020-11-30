312b20af92f97743de0ebdac6b9019c441ac6d9a
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-11-30 14:25:44 -0500

    graphs changes; data update

--- comfortzone.r
+++ comfortzone.r
@@ -12,14 +12,16 @@ source("config.r")
 
 
 # need to replace this later with automated import
-datafile <- "sources/NovasRoom.csv"
+dataname = "Lab42"
+
+datafile <- paste0("sources/", dataname, ".csv")
 comfort <- read_csv(datafile, 
                     col_types = cols(`Timestamp for sample frequency every 15 min` = col_datetime(format = "%Y-%m-%d %H:%M:%S"))
                     )
 colnames(comfort) = c("datetime","tempF", "humidity")
 
 # data size reduction by factor
-reductionfactor = 10
+reductionfactor = 1
 comfort <- comfort[seq(1,nrow(comfort),reductionfactor),]
 
 # generate extra data
@@ -34,11 +36,11 @@ comfort %>% mutate(
                     ) -> comfort
 
 
-comfort %>% ggplot() + aes(tempF,moisturecontent) + geom_point(alpha=.5) + 
+comfort %>%     ggplot() + aes(tempF,moisturecontent) + geom_point(alpha=.5) + 
                 xlab(tempFlabel) + ylab("Moisture content (mg water/kg air)") +
                 scale_x_continuous(limits=c(60,90), sec.axis=sec_axis(name=tempClabel,~ 5/9*(. -32))) + 
                 scale_y_continuous(limits=c(0,20)) + 
-                #facet_wrap(.~month) +  
+                facet_wrap(.~month) +  
                 geom_line(data=stdcurvedata %>% filter(relativehumidity==20), color="grey") + 
                 geom_line(data=stdcurvedata %>% filter(relativehumidity==40), color="grey") +
                 geom_line(data=stdcurvedata %>% filter(relativehumidity==60), color="grey") +
@@ -52,11 +54,8 @@ comfort %>% ggplot() + aes(tempF,moisturecontent) + geom_point(alpha=.5) +
                 annotate("text", x = 73, y = 18, label = "100%") +
                 annotate("text", x = 75, y = 1, label = "Comfort Zone") 
 
-ggsave("graphs/NovasRoom.pdf")
-write_csv(comfort, "data/NovasRoom.csv")
+graphname = paste0("graphs/", dataname, ".pdf")
+dataoutput = paste0("data/", dataname, ".csv")
 
-
-
-
-
-
+ggsave(graphname, height=8, width=11)
+write_csv(comfort, dataoutput)
